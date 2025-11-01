83ac7a09dd44824985e22dc09670609dc6874ae5
Clara Le <hey@clarale.com>
2025-11-01 10:00:39 +0100

    feat: create draft `Page` component to test live library

---
+++ src/components/Page/Page.stories.tsx
@@ -0,0 +1,31 @@
+import Page from ".";
+
+export default {
+  title: "My components/Page",
+  component: Page,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    title: { description: "The title of the Page", control: { type: "text" } },
+    children: {
+      description: "The children of the Page",
+      control: { type: "text" },
+    },
+  },
+};
+
+export const PageOne = {
+  args: {
+    title: "This is the Page One title.",
+    children: "This is the Page One children.",
+  },
+};
+
+export const PageTwo = {
+  args: {
+    title: "This is the Page One title.",
+    children: "This is the Page One children.",
+  },
+};
