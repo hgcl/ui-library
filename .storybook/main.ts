91c817448d60deadd9327bada880860cc64d22c9
Clara Le <hey@clarale.com>
2025-11-01 14:50:23 +0100

    build: set up storybook-design-token

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from "@storybook/nextjs-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: ["@storybook/addon-docs"],
+  addons: ["@storybook/addon-docs", "storybook-design-token"],
   framework: "@storybook/nextjs-vite",
 };
 export default config;
