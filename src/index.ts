577abc26bd336fe36947b12d70053307f7441ed7
Clara Le <hey@clarale.com>
2025-11-01 16:06:23 +0100

    fix: export `IconButton`

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ import "./globals.css";
 
 export { default as Page } from "./components/Page";
 export { default as Button } from "./components/Button";
+export { default as IconButton } from "./components/IconButton";
