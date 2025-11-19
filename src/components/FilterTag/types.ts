e209b1d47486b2a37f6545dfed3053b0c72416bf
Clara Le <hey@clarale.com>
2025-11-19 11:39:35 +0100

    fix: type of `FilterTag`

--- src/components/FilterTag/types.ts
+++ src/components/FilterTag/types.ts
@@ -1,8 +1,13 @@
+import { Dispatch, SetStateAction } from "react";
+
 export type FilterTagProps = {
   children?: React.ReactNode;
   label: string;
   id: string;
   isChecked: boolean;
-  handleFilterChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  handleFilterChange: (
+    event: React.ChangeEvent<HTMLInputElement>,
+    setFilters: Dispatch<SetStateAction<any>>
+  ) => void;
   groupName?: string;
 };
